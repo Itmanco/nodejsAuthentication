fff74d3af699217520ec289afc866e7b4d1fe1ab
itmanco <itmanco@gmail.com>
2024-05-04 22:47:50 +0900

    fist commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+
+mongoose.connect('mongodb://localhost:27017/authDemo')
+.then(()=> {
+    console.log("MONGO CONNECTION OPEN!!");
+})
+.catch(err => {
+    console.log("OH NO MONGO CONNECTION ERROR!!");
+    console.log(err);
+})
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({extended: true}));
+app.use(session({secret: 'notagoodsecret',resave: true,
+saveUninitialized: true}));
+
+const requireLogin = (req, res, next) => {
+    if(!req.session.user_id){
+        return res.redirect('/login');
+    }
+    next();
+}
+
+app.get('/', (req, res) => {
+    res.send('THIS IS THE HOME PAGE!!')
+})
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    const user = new User({ username, password });
+    await user.save();
+    req.session.user_id = user._id;
+    res.redirect('/')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const foundUser = await User.findAndValidate(username, password);
+    if(foundUser){
+        req.session.user_id = foundUser._id;
+        return res.redirect('/secret');
+    }
+
+    res.redirect('/login');
+})
+
+app.post('/logout', (req,res) => {
+    // req.session.user_id = null;
+    req.session.destroy();
+    res.redirect('/login');
+})
+
+app.get('/secret', requireLogin, (req,res) => {
+    res.render('secret')
+})
+
+app.get('/topsecret', requireLogin, (req,res) => {
+    res.send('TOPSECRET PAGE');
+})
+
+app.listen(3000, ()=>{
+    console.log('SERVING at 3000');
+})
---
+++ readme.md
@@ -0,0 +1,30 @@
+# nodejs express Authentication
+
+A basic functional project that implements authentication in a nodejs project using express and manages cookies to create a user session
+The project was implemented using nodejs
+
+components used in this project:
+ user login
+- [x] nodejs
+- [x] express
+- [x] mongo
+- [x] mongoose
+- [x] bcrypt
+- [x] express-session
+
+
+## Installation
+
+First clone the repository from Github and switch to the new directory:
+
+    $ git clone https://github.com/Itmanco/nodejsAuthentication.git
+    $ cd {{ project_name }}
+
+
+Install project dependencies:
+
+    $ node i express mongoose bcrypt express-session
+
+This project requires a mongo instance running on localhost
+
+to start the express service use: node index.js
